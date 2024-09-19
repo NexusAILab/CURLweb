de3bc274ff5975ee74678f7b10084af7ac0abbf5
Pushkar Singh <84075300+PushkarOP@users.noreply.github.com>
2024-09-19 16:58:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,5 +55,4 @@ def run_curl():
 if __name__ == '__main__':
     # Use the PORT environment variable or default to 5000
     port = int(os.environ.get('PORT', 5000))
-    logger.info(f"Starting the app on port {port}")
     app.run(host='0.0.0.0', port=port, debug=False)
